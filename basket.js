2f7b0b92378cecd7c683cfeaa174f8afd7f30ca8
khayalnb <xeyal.binnetov@profit.az>
2023-10-08 21:44:06 +0400

    data change directory import data.js

--- basket.js
+++ basket.js
@@ -1,3 +1,6 @@
+import productsData from './data.js'
+
+
 class Product {
     constructor(id, title, price, category, image) {
         this.id = id;
@@ -140,36 +143,7 @@ class Basket {
     }
 }
 
-const productsData = [
-    {
-        "id": 1,
-        "title": "Fits 15 Laptops",
-        "price": 109.95,
-        "category": "Men's clothing",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    },
-    {
-        "id": 2,
-        "title": "Slim Fit T-Shirts ",
-        "price": 22.3,
-        "category": "Men's clothing",
-        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    },
-    {
-        "id": 3,
-        "title": "Mens Cotton Jacket",
-        "price": 55.99,
-        "category": "Men's clothing",
-        "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-    },
-    {
-        "id": 4,
-        "title": "Mens Casual Slim Fit",
-        "price": 15.99,
-        "category": "Men's clothing",
-        "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-    },
-];
+
 
 const products = productsData.map((data) => new Product(data.id, data.title, data.price, data.category, data.image));
 
