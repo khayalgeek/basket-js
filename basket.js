a5e6e7fc2777e7afdfe1e4d64c3b8d94552c2c20
khayalnb <xeyal.binnetov@profit.az>
2023-10-08 21:21:12 +0400

    add remove basket item

--- basket.js
+++ basket.js
@@ -27,53 +27,79 @@ class Basket {
     async loadBasketItem() {
         const basketItemContainer = document.getElementById('basket-products');
         const basketItems = await this.loadBasketFromLocalStorage();
-        basketItems.forEach(item => {
-            let html = "";
-            html =
-                `
-                <div class="col-sm-11 m-2">
-                    <div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4 d-flex justify-content-center align-items-center p-2">
-                                <img src="${item.image}"
-                                    class="img-fluid rounded-start" alt="...">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${item.title}</h5>
-                                    <p class="card-text">Qiymət: ${item.price} AZN</p>
-                                    <p class="card-text">Kateqoriya: ${item.category}</p>
-                                    <p class="card-text">Əlavə olunub: <small class="text-body-secondary">${item.date}</small></p>
+        let html = "";
+        if (basketItems.length > 0) {
+
+            await basketItems.forEach(item => {
+
+                html =
+                    `
+                            <div class="col-sm-11 m-2 product-container">
+                                <div class="card mb-3" style="max-width: 540px;">
+                                    <div class="row g-0">
+                                        <div class="col-md-4 d-flex justify-content-center align-items-center p-2">
+                                            <img src="${item.image}"
+                                                class="img-fluid rounded-start" alt="...">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${item.title}</h5>
+                                                <span class="card-text d-block mb-1">Qiymət: ${item.price} AZN</span>
+                                                <span class="card-text d-block mb-1">Kateqoriya: ${item.category}</span>
+                                                <span class="card-text d-block mb-1">Əlavə olunub: <small class="text-body-secondary">${item.date}</small></p>
+                                                <span class="card-text d-block mb-1">Toplam: ${item.count * item.price} AZN</span>
+                                                <div class="mt-2 w-100 d-flex justify-content-between align-items-center">
+                                                    <div class="d-flex align-items-center">
+                                                        <button data-id="${item.id}" class="btn btn-primary minus-btn">-</button>
+                                                        <span  class="border border-secondary item-count ms-2 me-2">${item.count}</span>
+                                                        <button data-id="${item.id}" class="btn btn-primary plus-btn">+</button>
+                                                    </div>
+                                                    <button data-id="${item.id}" class="btn btn-warning remove-btn">Sil</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                     `
+                basketItemContainer.innerHTML += html;
+            })
+        } else {
+            html = `
+               <div>
+                    <div class="alert alert-warning" role="alert">
+                        Səbətdə məhsul yoxdur !
                     </div>
-                </div>
+               </div>
             `
             basketItemContainer.innerHTML += html;
-        })
+        }
 
-        console.log(basketItems);
     }
 
     updateBasketCount(count) {
         const basketCount = document.getElementById('basket-count');
         basketCount.innerText = count;
     }
+
     formatDate(date) {
-        const year = date.getFullYear().toString().slice(-2); 
-        const month = String(date.getMonth() + 1).padStart(2, '0'); 
-        const day = String(date.getDate()).padStart(2, '0'); 
+        const year = date.getFullYear().toString().slice(-2);
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
         const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0'); 
-        
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+
         return `${day}.${month}.${year} ${hours}:${minutes}`;
     }
+
     async addToBasket(productId) {
+        const basketItemContainer = document.getElementById('basket-products');
+
         const basketSession = await this.loadBasketFromLocalStorage();
         const isProductInBasket = basketSession.some((item) => item.id === productId);
 
         if (!isProductInBasket) {
+            basketItemContainer.innerHTML = "";
             const product = products.find((p) => p.id === productId);
 
             if (product) {
@@ -85,12 +111,27 @@ class Basket {
 
                 basketSession.push(newProduct);
                 await this.saveBasketToLocalStorage(basketSession);
+                await this.loadBasketItem()
                 this.updateBasketCount(basketSession.length);
+
                 return;
             }
 
         }
+
         alert(Basket.HAVE_PRODUCT_MESSAGE);
+    }
+    async removeToBasket(productId) {
+        const basketItemContainer = document.getElementById('basket-products');
+        const basketSession = await this.loadBasketFromLocalStorage();
+        const removeBasketItem = basketSession.filter(product => product.id !== productId);
+        if (removeBasketItem) {
+            basketItemContainer.innerHTML = "";
+            await this.saveBasketToLocalStorage(removeBasketItem);
+            this.updateBasketCount(basketSession.length);
+            return;
+        }
+
     }
 
     async saveBasketToLocalStorage(basketData) {
@@ -128,7 +169,6 @@ const productsData = [
         "category": "Men's clothing",
         "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
     },
-
 ];
 
 const products = productsData.map((data) => new Product(data.id, data.title, data.price, data.category, data.image));
@@ -164,8 +204,25 @@ products.forEach((product) => {
 const addBtn = document.querySelectorAll('.card-link');
 
 addBtn.forEach((btn) => {
-    btn.addEventListener('click', function () {
+    btn.addEventListener('click', async function () {
         let dataId = Number(this.getAttribute('data-id'));
-        basket.addToBasket(dataId);
+        await basket.addToBasket(dataId);
     });
 });
+
+
+let basketProductContainer =document.getElementById('basket-products');
+
+basketProductContainer.addEventListener('click',async function(e){
+    e.preventDefault();
+    if(e.target.className.includes("remove-btn")){
+        let dataId = Number(e.target.getAttribute('data-id'));
+        let productContainer = e.target.closest('.product-container');
+        if (productContainer) {
+            productContainer.remove();
+            await basket.removeToBasket(dataId);
+            await basket.loadBasketItem();
+            await basket.loadBasketCount();
+        }
+    }
+})
